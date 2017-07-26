8769e1748c21d42b8cb2dd9baf542c0f325e5fd7
webxiaoma <webxiaoma@qq.com>
2017-07-26 22:27:37 +0800

    change

--- 日常插件效果/数字跳动/bounce.js
+++ 日常插件效果/数字跳动/bounce.js
@@ -1,18 +1,81 @@
 
 // 数字跳动 
-// athour: webxioama
+// author: webxioama
 
 (function(w,O,obj){
    var mBounce = obj.mBounce;
-   O.prototype[mBounce] = function(time){
-   	   var time = time || 500;
-       var mun = this.innerHTML;
-       var arrayMun = mun.split(".");
-       var inte = Number(arrayMun[0]);
-       var folt = Number(arrayMun[1]);
-       
-       console.log(arrayMun)
-   }
+  
+
+     O.prototype[mBounce] = function(e){
+       var that = this;
+
+       function fun(time,that){
+         var that = that;
+         var time = time || 80;
+         var arrayMun = that.innerText.split(".");
+         var intea = arrayMun[0]; // 整数部分
+         var folt = arrayMun[1]; // 小数部分
+         var newArray = [];  
+         var initArray = [];
+         var len=Math.ceil(intea.length/2);
+         var fen = intea.length%2; // 能否两两等分
+         var c = 0;
+         for(var i=0;i<len;i++){
+            var start = 2*i;
+            newArray.push(intea.substr(start,2));
+            initArray.push(0);
+         }
+         // setInterval(objFun.initOne,time)
+         numSet = setInterval(function(){
+           if(fen == 0){ // 第一个为两位数
+              for(var i=0,le=newArray.length; i<le; i++){
+                  if(c<10){
+                    initArray[i] = "0" + c;
+                    c++; 
+                  }else if(c<99 && c>=10){
+                    initArray[i] = c;
+                    c++;
+                  }else{
+                    c = 0;
+                  }  
+                 arrayMun[1] = c;
+              }
+
+           }else{ // 第一个为一位数
+              for(var i=1,le=newArray.length; i<le; i++){
+                  if(c<10){
+                    initArray[i] = "0" + c;
+                    initArray[0] = c;
+                    c++; 
+                  }else if(c<99 && c>=10){
+                    initArray[i] = c;
+                    c++;
+                  }else{
+                    c = 0;
+                  }    
+              }
+
+              arrayMun[1] = c;
+           }
+
+           
+        
+          that.innerText = initArray.join("")+"."+ arrayMun[1];
+          close = setTimeout(function(){
+            clearInterval(numSet);
+            for(var i=0,le=newArray.length; i<le; i++){
+               initArray[i] = newArray[i];
+               that.innerText = initArray.join("")+"."+ folt;
+            }
+            clearTimeout(close)
+          },1000)
+          
+         },time)
+          
+       }
+
+       return new fun(that);
+     }
 
 })(window,Object,{
 	mBounce:"mBounce", // 数字跳动
