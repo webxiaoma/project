2517d110b7f6705e5a0b3799dc36ccfec6c9b46c
webxiaoma <1376557466@qq.com>
2017-07-26 18:05:26 +0800

    change

---
+++ 日常插件效果/数字跳动/bounce.js
@@ -0,0 +1,19 @@
+
+// 数字跳动 
+// athour: webxioama
+
+(function(w,O,obj){
+   var mBounce = obj.mBounce;
+   O.prototype[mBounce] = function(time){
+   	   var time = time || 500;
+       var mun = this.innerHTML;
+       var arrayMun = mun.split(".");
+       var inte = Number(arrayMun[0]);
+       var folt = Number(arrayMun[1]);
+       
+       console.log(arrayMun)
+   }
+
+})(window,Object,{
+	mBounce:"mBounce", // 数字跳动
+}) 
