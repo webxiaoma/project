09cda191928150e6845eb106aea4de5c24d6f35d
webxiaoma <1376557466@qq.com>
2017-08-17 10:00:29 +0800

    change

--- JQuery/滚动加载动画/mScroll.js
+++ JQuery/滚动加载动画/mScroll.js
@@ -1,16 +1,17 @@
-(function(rootObj){
 
-	// ********************  滚动页面加载动画 start**********************
-    
-    var mScroll=rootObj.mScroll; 
+// ********************  滚动页面加载动画 start**********************
+(function(rootObj){
+     var mScroll=rootObj.mScroll; 
      $.fn[mScroll]=function(topPX,funOne,funTwo){
+
              var __def__={
               topPX:0,
               funOne:function(){}, // 到达时的回调函数
               funTwo:function(){}, // 离开时的回调函数
              }
+             var newTopPX = Number(parseInt(topPX));
              var __pro__={
-              topPX:topPX,
+              topPX:newTopPX,
               funOne:funOne,
               funTwo:funTwo,
              }
@@ -21,7 +22,9 @@
                 
                $(window).scroll(function(){
               
-                   var scro= $("body,heml").scrollTop()
+     
+                   var scro= $("body,html").scrollTop()
+                            // alert(scro)
                    var top = that.position().top-scro; // 到达动画开始位置
                    var height=that.height()+that.position().top; //离开动画结束位置
                    
@@ -31,10 +34,12 @@
                       that.funTwo.call(that);
                    }
                })
+
      }
 
-// ********************  滚动页面加载动画 end**********************
 
 })({
    mScroll:"mScroll",           // 滚动页面加载动画
 })
+
+// ********************  滚动页面加载动画 end**********************
